022731bf146958804d9141382eccbca94e916112
Gene-94 <yevheniykushnirenko@gmail.com>
2026-04-08 02:49:47 +0100

    mlx wisper pipping data to mlx based gemma4 (26b MoE), for voice based interaction (voice -> text)

--- vad.py
+++ vad.py
@@ -1,4 +1,6 @@
 import sounddevice, soundfile, numpy, mlx_whisper
+from mlx_vlm import load, generate
+from mlx_vlm.prompt_utils import apply_chat_template
 
 def get_audio():
     sample_rate = 16000
@@ -28,6 +30,27 @@ def transcribe_audio(filename):
     )
     return result["text"].strip()
 
+def think(transcribed_input):
+   
+    model, processor = load("mlx-community/gemma-4-26B-A4B-it-heretic-4bit",
+        chat_template_kwargs={"enable_thinking": True},)
+
+
+    prompt = apply_chat_template(
+        processor, model.config, transcribed_input
+    )
+
+    result = generate(
+        model=model,
+        processor=processor,
+        prompt=prompt,
+        max_tokens=1000,
+        temperature=1.0,
+        top_p=0.95,
+        top_k=64,
+    )
+    print("\n"+result.text)
+
 if __name__ == "__main__":
     scrib = transcribe_audio(get_audio())
-    print(f"So you '{scrib}', is that it?")
+    think(scrib)
