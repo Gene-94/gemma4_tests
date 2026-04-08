306e4e32ee98e5cae00cf0585ef6b33ffbb9c464
Gene-94 <yevheniykushnirenko@gmail.com>
2026-04-08 02:34:03 +0100

    transcribing fixed time segments works perfectly

--- vad.py
+++ vad.py
@@ -1,8 +1,8 @@
-import sounddevice, soundfile, numpy
+import sounddevice, soundfile, numpy, mlx_whisper
 
-def test_mic():
+def get_audio():
     sample_rate = 16000
-    duration = 10
+    duration = 5
     channels = 1
 
     print(f"Recording for {duration} seconds...")
@@ -19,5 +19,15 @@ def test_mic():
     filename = "test_audio.wav"
     soundfile.write(filename, audio_data, sample_rate)
 
+    return filename
+
+def transcribe_audio(filename):
+    result = mlx_whisper.transcribe(
+        "./"+filename,
+        path_or_hf_repo="mlx-community/whisper-large-v3-turbo"
+    )
+    return result["text"].strip()
+
 if __name__ == "__main__":
-    test_mic()
+    scrib = transcribe_audio(get_audio())
+    print(f"So you '{scrib}', is that it?")
